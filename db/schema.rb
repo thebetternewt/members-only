5cf37c9ddb89e01117113447bf1d0e9df640f9fe
Chris Eady <chris.eady1@gmail.com>
2016-08-03 21:53:24 -0500

    added sessions controller and login form

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804020710) do
+ActiveRecord::Schema.define(version: 20160804021253) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
+    t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
